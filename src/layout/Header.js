9e014cbe646965cad7df74dfb48e64ff420d6e02
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-12-24 01:46:45 +0600

    menu responsiveness checked

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -24,24 +24,24 @@ const Header = () => {
 
     return (
         <div className=''>
-            <div className='text-base-100 bg-[#1E1F29] py-2 px-10 flex justify-between'>
+            <div className='text-base-100 bg-[#1E1F29] py-2 px-5 md:px-10 flex justify-between'>
                 <div className=' '>
-                    <ul className='flex'>
+                    <ul className='md:flex'>
                         <li className='font-semibold hover:text-primary mr-2'><a className='flex items-center' href=""><FaPhoneAlt color='#ff1e00' /> +021-95-51-84</a></li>
                         <li className='font-semibold hover:text-primary mr-2'><a className='flex items-center' href=""><FaEnvelope color='#ff1e00' /> email@email.com</a></li>
                         <li className='font-semibold hover:text-primary mr-2'><a className='flex items-center' href=""><MdLocationOn color='#ff1e00' /> 1734 Stonecoal Road</a></li>
                     </ul>
                 </div>
-                <div className='flex'>
+                <div className='md:flex'>
                     <p className='font-semibold hover:text-primary mr-2 flex items-center'><IoLogoUsd color='#ff1e00' />USD</p>
                     <p className='font-semibold hover:text-primary mr-2 flex items-center'><BsPersonCircle color='#ff1e00' />My Account</p>
                 </div>
             </div>
-            <div className='bg-[#15161D] py-3 px-10 md:flex flex-row justify-between items-center border-b-2 border-primary'>
-                <div className='basis-1/4'>
+            <div className='bg-[#15161D] py-3 px-10 lg:flex flex-row justify-between items-center border-b-2 border-primary'>
+                <div className='basis-1/4 flex justify-center lg:justify-start pb-5 lg:pb-0'>
                     <p className='text-6xl font-bold text-base-100'>Electro</p>
                 </div>
-                <div className='basis-2/4 flex justify-between bg-base-100 rounded-full py-1 px-3'>
+                <div className='basis-2/4 lg:flex justify-between bg-base-100 rounded-full py-1 px-3 hidden'>
                     <div className=''>
                         <select className='' name="" id="">
                             <option value="All Category" selected>All Category</option>
@@ -88,7 +88,7 @@ const Header = () => {
                 </div>
             </div>
             <div className='bg-base-100 py-4 border-b-2 border-secondary'>
-                <ul className='flex justify-center'>
+                <ul className='md:flex justify-center'>
                     <li className='mx-5 text-lg font-semibold'>Home</li>
                     <li className='mx-5 text-lg font-semibold'>Hot Deals</li>
                     <li className='mx-5 text-lg font-semibold'>Laptop</li>
--- src/pages/Home/HotDeals.js
+++ src/pages/Home/HotDeals.js
@@ -4,13 +4,13 @@ import bgImage from '../../assets/images/hotdeal.png'
 
 const HotDeals = () => {
     return (
-        <div className='flex justify-center py-20' style={{backgroundImage: `url(${bgImage})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'scroll'}}>
+        <div className='flex justify-center py-20 px-5 lg:px-10' style={{backgroundImage: `url(${bgImage})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'scroll'}}>
             <div>
-                <ul className='flex justify-center pb-5'>
-                    <li className='mr-3 w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Days</span> </p></li>
-                    <li className='mr-3 w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Hours</span> </p></li>
-                    <li className='mr-3 w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Mins</span> </p></li>
-                    <li className='mr-3 w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Secs</span> </p></li>
+                <ul className='grid lg:grid-cols-4 grid-cols-2 gap-5  pb-5'>
+                    <li className=' w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Days</span> </p></li>
+                    <li className=' w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Hours</span> </p></li>
+                    <li className=' w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Mins</span> </p></li>
+                    <li className=' w-24 h-24 rounded-full bg-primary text-base-100 flex justify-center items-center'> <p className='text-center text-xl font-semibold'>10 <span className='block text-sm text-center uppercase'>Secs</span> </p></li>
                 </ul>
                 <h2 className='text-center text-4xl font-bold text-neutral uppercase'>hot deal this week</h2>
                 <h3 className='text-center text-xl uppercase py-5'>New Collection Up to 50% OFF</h3>
